827e7a6b74c3f887029b7d54aff0d54c93e57b43
bssmsi <bharathsshroff@gmail.com>
2025-06-11 14:51:04 +0000

    config fixes for dynamic web app

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com', // Replace with your image host
+      },
+      {
+        protocol: 'https',
+        hostname: 'another-image-cdn.com', // Add more as needed
+      },
+    ],
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
